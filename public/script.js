d421b7ba3f41e043b22b18bce0201413f99229d8
x404 <kronos2003@gmail.com>
2017-06-26 19:04:47 +0300

    iterate cart items in template (раздел4, лекция 19)

--- public/script.js
+++ public/script.js
@@ -1,1 +1,19 @@
-console.log('It works.');
+new Vue({
+	el: '#app',
+	data: {
+		total: 1,
+		items : [
+			{ title : 'Item 1' },
+			{ title : 'Item 2' },
+			{ title : 'Item 3' }
+		],
+		cart: []
+	},
+	methods : {
+		addItem: function(index){
+			console.log(this.items[index]);
+			this.total += 9.99;
+			this.cart.push(this.items[index]);
+		}
+	}
+});
