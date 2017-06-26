9c5ec1358e93a44c2924519b7191ea053471c221
x404 <kronos2003@gmail.com>
2017-06-26 20:15:05 +0300

    Добавлена логика работы с количеством товара в корзине

--- public/script.js
+++ public/script.js
@@ -3,23 +3,34 @@ new Vue({
 	data: {
 		total: 1,
 		items : [
-			{ title : 'Item 1' },
-			{ title : 'Item 2' },
-			{ title : 'Item 3' }
+			{ id: 1, title : 'Item 1' },
+			{ id: 2, title : 'Item 2' },
+			{ id: 3, title : 'Item 3' }
 		],
 		cart: []
 	},
 	methods : {
 		addItem: function(index){
-			console.log(this.items[index]);
+			// console.log(this.items[index]);
 			this.total += 9.99;
 			var item = this.items[index];
+			var found = false;
 
-			this.cart.push({
-				title: item.title,
-				qty: 1
-				// this.items[index]
-			});
+
+			for (var i = 0; i < this.cart.length; i++){
+				if (this.cart[i].id === item.id){
+					found = true;
+					this.cart[i].qty++;
+				}
+			}
+			if (!found){
+				this.cart.push({
+					id : item.id, 
+					title: item.title,
+					qty: 1
+					// this.items[index]
+				});				
+			}
 		}
 	}
 });
