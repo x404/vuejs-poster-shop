56a67ca73f56f53464e2b18bb9552971ae19c73a
x404 <kronos2003@gmail.com>
2017-06-28 13:26:34 +0300

    Добавлена кнопка поиска. Создана v-model

--- public/script.js
+++ public/script.js
@@ -9,9 +9,13 @@ new Vue({
 			{ id: 2, title : 'Item 2' },
 			{ id: 3, title : 'Item 3' }
 		],
-		cart: []
+		cart: [],
+		search: ''
 	},
 	methods : {
+		onSubmit: function(){
+			console.log(this.search);
+		},
 		addItem: function(index){
 			// console.log(this);
 			this.total += PRICE;
@@ -57,7 +61,8 @@ new Vue({
 					}
 				}
 			}
-		},
+		}
+
 	},
 	filters : {
 		currency: function(price){
